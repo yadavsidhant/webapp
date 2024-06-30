dc500281ac916ca4a06d0a74ebd107136e78e8e5
Sidhant Yadav <83953899+yadavsidhant@users.noreply.github.com>
2024-06-30 13:11:53 +0530

    Create authMiddleware.js

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const User = require('../models/User');
+
+dotenv.config();
+
+module.exports = async (req, res, next) => {
+    const token = req.header('x-auth-token');
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token, authorization denied' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: 'Token is not valid' });
+    }
+};
